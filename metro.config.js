cd6ad4ed1b51f957431b463516c988ffeff72317
Eniola Oladejo <enioladejo1725@gmail.com>
2024-11-16 10:54:38 +0100

    bug fix

--- metro.config.js
+++ metro.config.js
@@ -1,19 +1,35 @@
-const { getDefaultConfig } = require.resolve("react-native-svg-transformer/expo")
+const { getDefaultConfig } = require('expo/metro-config');
 
-module.exports = (() => {
-  const config = getDefaultConfig(__dirname);
+const defaultConfig = getDefaultConfig(__dirname);
 
-  const { transformer, resolver } = config;
+defaultConfig.transformer.babelTransformerPath = require.resolve(
+  'react-native-svg-transformer/expo'
+);
+defaultConfig.resolver.assetExts = defaultConfig.resolver.assetExts.filter(
+  (ext) => ext !== 'svg'
+);
+defaultConfig.resolver.sourceExts.push('svg');
 
-  config.transformer = {
-    ...transformer,
-    babelTransformerPath: require.resolve("react-native-svg-transformer/expo")
-  };
-  config.resolver = {
-    ...resolver,
-    assetExts: resolver.assetExts.filter((ext) => ext !== "svg"),
-    sourceExts: [...resolver.sourceExts, "svg"]
-  };
+module.exports = defaultConfig;
 
-  return config;
-})();
+// const { getDefaultConfig } = require.resolve(
+//   'react-native-svg-transformer/expo'
+// );
+
+// module.exports = (() => {
+//   const config = getDefaultConfig(__dirname);
+
+//   const { transformer, resolver } = config;
+
+//   config.transformer = {
+//     ...transformer,
+//     babelTransformerPath: require.resolve('react-native-svg-transformer/expo'),
+//   };
+//   config.resolver = {
+//     ...resolver,
+//     assetExts: resolver.assetExts.filter((ext) => ext !== 'svg').concat('png'),
+//     sourceExts: [...resolver.sourceExts, 'svg'],
+//   };
+
+//   return config;
+// })();
